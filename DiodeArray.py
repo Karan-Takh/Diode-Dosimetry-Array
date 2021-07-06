619e15cf172eded0cd2b3b5bf2236099c507d253
kwiratan <kwiratan@terpmail.umd.edu>
2021-07-06 17:39:17 -0400

    Color change test

--- DiodeArray.py
+++ DiodeArray.py
@@ -1,19 +1,19 @@
 from graphviz import Graph
 
-num = input('Enter word: ')
+volt6 = 7
 
 h = Graph('html_table')
 h.attr('node', shape='rectangle', bgcolor="brown:green")
 h.node('Diode Array', label=f'''<<TABLE>
  <TR>
-   <TD>Diode 1</TD>
+   <TD border="3" style="radial" bgcolor="yellow"  gradientangle="60">Diode 1</TD>
    <TD>Diode 2</TD>
    <TD>Diode 3</TD>
  </TR>
  <TR>
    <TD>Diode 4</TD>
    <TD>Diode 5</TD>
-   <TD>{num}</TD>
+   <TD>Diode 6: {volt6} V</TD>
  </TR>
 </TABLE>>''')
 
